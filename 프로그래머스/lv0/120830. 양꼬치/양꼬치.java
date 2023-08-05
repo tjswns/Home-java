14de3c888f91d9204b7406a2777532c13414287c
tjswns <111962386+tjswns@users.noreply.github.com>
2023-08-05 22:05:24 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = (12000 * n) + ((k-(n/10)) * 2000);
+        
+        return answer;
+    }
+}
