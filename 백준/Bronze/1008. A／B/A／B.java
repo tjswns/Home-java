3d80ef9ac8704b8083bb77455426f5fc95894cf2
tjswns <111962386+tjswns@users.noreply.github.com>
2024-01-29 12:28:11 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        Double A = in.nextDouble();
+        Double B = in.nextDouble();
+        
+        System.out.print(A / B);
+    }
+}
